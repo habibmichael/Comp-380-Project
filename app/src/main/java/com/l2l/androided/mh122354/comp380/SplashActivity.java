5d58f647ea89e822e59a49d5734e8a85a459f95b
Michael Habib <Michael Habib>
2016-07-27 19:06:38 -0700

    Initial

---
+++ app/src/main/java/com/l2l/androided/mh122354/comp380/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.l2l.androided.mh122354.comp380;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
